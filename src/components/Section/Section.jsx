c79a59890b602616e3a41a181bdf8408bf787ae4
Zerossik <zeross19940303@gmail.com>
2023-07-02 00:14:24 +0300

    fix

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
 import { SectionEl } from './Section.styled';
-import { Outlet } from 'react-router-dom';
 
-export function Section({ title, children }) {
+export function Section({ children }) {
   return <SectionEl>{children}</SectionEl>;
 }
 Section.propTypes = {
-  title: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.exact).isRequired,
 };
