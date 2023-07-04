8815551851f4fa687b647ddabda6d059487fbcba
Zerossik <zeross19940303@gmail.com>
2023-07-04 03:18:48 +0300

    add Home Page

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,8 @@
+import { HomeWrap } from './Home.styled';
+export const Home = () => {
+  return (
+    <HomeWrap>
+      <h1>Welcome to phoneBook</h1>
+    </HomeWrap>
+  );
+};
---
+++ src/page/Home/Home.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HomeWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+`;
